684a2a7bf8c02a83becd51c3e6f9afcefc27a2ae
njorokel <kelvinmkoloni17@gmail.com>
2023-03-14 15:50:20 +0300

    app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+
+import sys
+import time
+number=int(input("Enter number\n"))
+if number <= 0:
+        print("negative numbers are not allowed")
+        time.sleep(2)
+        sys.exit(0)
+else:
+        print('number',number)    
+
+
